454883f7983d8c44655b03951a4648f1e8bda632
Heain <hhi0128h@gmail.com>
2018-11-29 09:05:03 +0900

    commit

--- Fire/app/src/main/java/com/rnc/fire/MainActivity.java
+++ Fire/app/src/main/java/com/rnc/fire/MainActivity.java
@@ -3,6 +3,9 @@ package com.rnc.fire;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,23 +17,21 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView webView;
+    private WebSettings webSettings;
+    private String url;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        FirebaseDatabase database = FirebaseDatabase.getInstance();
-//        DatabaseReference myRef  = database.getReference("sensor");
-//        myRef.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//
-//            }
-//        })
+        webView = (WebView)findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        url = "http://168.131.150.80:4567/sensor/";
+
+        webView.loadUrl(url);
     }
 }
